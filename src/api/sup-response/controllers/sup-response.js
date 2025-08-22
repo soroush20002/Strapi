974073e99950874810dd64b942eb75d7c0d365ed
unknown <soroushnassaj@gmail.com>
2025-08-22 15:16:05 +0430

    sup-resController

--- src/api/sup-response/controllers/sup-response.js
+++ src/api/sup-response/controllers/sup-response.js
@@ -1,9 +1,19 @@
 'use strict';
 
-/**
- * sup-response controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::sup-response.sup-response');
+module.exports = createCoreController('api::sup-response.sup-response', ({ strapi }) => ({
+  async find(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.unauthorized('Unauthorized');
+    }
+    let filters = ctx.query.filters || {};
+    if (!user.role || user.role.name !== 'Admin') {
+      filters.ui = { $eq: user.id };
+    }
+    ctx.query.filters = filters;
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
--- src/api/sup/controllers/sup.js
+++ src/api/sup/controllers/sup.js
@@ -1,9 +1,34 @@
 'use strict';
 
-/**
- * sup controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::sup.sup');
+module.exports = createCoreController('api::sup.sup', ({ strapi }) => ({
+  async find(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.unauthorized('unauthorized');
+    }
+    let filters = ctx.query.filters || {};
+    if (!user.role || user.role.name !== 'Admin') {
+      filters.ui = { $eq: user.id }; 
+    }
+    ctx.query.filters = filters;
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+  async create(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.unauthorized('Unauthorized');
+    }
+    const requestData = ctx.request.body.data;
+    if (!user.role || user.role.name !== 'Admin') {
+      requestData.ui = user.id;
+    }
+    ctx.request.body.data = requestData;
+    const { data, meta } = await super.create(ctx);
+    return { data, meta };
+  }
+}));
+
+
